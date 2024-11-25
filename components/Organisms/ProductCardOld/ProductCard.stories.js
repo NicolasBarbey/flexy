591b6bfb9a471e4a2272a572b41661aacb18b09c
lnormandon <lnormandon@openstudio.fr>
2024-11-25 15:46:23 +0100

    Search Item

--- components/Organisms/ProductCardOld/ProductCard.stories.js
+++ components/Organisms/ProductCardOld/ProductCard.stories.js
@@ -1,31 +1,7 @@
-import Search from './Search.html.twig';
-import Order from './Order.html.twig';
 import PurchaseFunnel from './PurchaseFunnel.html.twig';
-import AddToCartConfirmation from './AddToCartConfirmation.html.twig';
-import RemoveProduct from './RemoveProduct.html.twig';
-import progressBar from './RemoveProduct.js';
 
 export default {
   title: 'Design System/Organisms/ProductCardOld'
-};
-
-export const search = {
-  render: (args) => Search(args),
-  args  : {
-    productTitle: 'Nom du produit',
-    price       : '1000,00€'
-  }
-};
-
-export const order = {
-  render: (args) => Order(args),
-  args  : {
-    productTitle       : 'Nom du produit',
-    orderSecondaryTitle: 'Titre secondaire',
-    size               : 'S-34/36',
-    quantity           : 1,
-    price              : '50,00€'
-  }
 };
 
 export const purchaseFunnel = {
---
+++ components/Organisms/SearchItem/SearchItem.stories.js
@@ -0,0 +1,19 @@
+import SearchItem from './SearchItem.html.twig';
+
+export default {
+  title: 'Design System/Organisms/SearchItem'
+};
+
+export const base = {
+  render  : (args) => SearchItem(args),
+  args    : {
+    vertical      : false,
+    img           : { url: '/images/placeholder2.webp', alt: '' },
+    productTitle  : 'Nom du produit',
+    secondaryTitle: 'Titre secondaire',
+    price         : '50,00€',
+    isPromo       : false,
+    promoPrice    : '30,00€'
+  },
+  argTypes: {}
+};
