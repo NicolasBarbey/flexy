779ee0dd9a017ea05abffcad518a5eb7c7129446
Léa Normandon <lnormandon@openstudio.fr>
2025-05-07 10:27:03 +0200

    Feat/pickup (#80)
    * init pickuppoint map
    
    * use api instead of controller twig for delivery modules listing
    
    * pickup points location
    
    * change pin if selected
    
    ---------
    
    Co-authored-by: Rob <32717550+robinallezard@users.noreply.github.com>

--- components/Organisms/Card/PickupPoint/PickupPoint.js
+++ components/Organisms/Card/PickupPoint/PickupPoint.js
@@ -1,12 +1,11 @@
 export function pickupPointHours() {
-  const buttons = document.querySelectorAll('.PickupPoint-hours');
-
-  buttons.forEach((button) => {
-    button.addEventListener('click', function() {
-      const parent       = this.closest('.PickupPoint');
+  const pickupPointModule = document.querySelector('.PickupPointModule');
+  pickupPointModule.addEventListener('click', function (event) {
+    if (event.target.matches('.PickupPoint-hours')) {
+      const parent = event.target.closest('.PickupPoint');
       const hoursListing = parent.querySelector('.PickupPoint-hoursListing');
       hoursListing.classList.toggle('md:hidden');
-    });
+    }
   });
 }
 
@@ -14,7 +13,7 @@ export function pickupPoint() {
   const inputs = document.querySelectorAll('.PickupPoint input[type="radio"]');
 
   inputs.forEach((input) => {
-    input.addEventListener('change', function() {
+    input.addEventListener('change', function () {
       const allPickupPoints = document.querySelectorAll('.PickupPoint');
       allPickupPoints.forEach((pickupPoint) => {
         pickupPoint.classList.remove('selected');
