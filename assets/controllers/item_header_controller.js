86fc1723a49591105abbac92f7923edcc1fc1c41
Robin Morel <rmorel@openstudio.fr>
2025-05-27 17:24:44 +0200

    menu

---
+++ assets/controllers/item_header_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from '@hotwired/stimulus';
+
+class ItemHeaderController extends Controller {
+  static targets = ['toggler'];
+
+  connect() {
+    this.element.addEventListener('keydown', ({ key }) => {
+      if (key === 'Escape') {
+        this.togglerTarget.classList.remove('is-open');
+        this.togglerTarget.focus();
+      }
+    });
+
+    document.addEventListener('click', (event) => {
+      if (!this.element.contains(event.target)) {
+        this.togglerTarget.classList.remove('is-open');
+      }
+    });
+  }
+  toggle({ target }) {
+    target.classList.toggle('is-open');
+  }
+}
+
+export default ItemHeaderController;
