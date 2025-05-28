10b3063311ecda108a9eb948f5be21b9af02e319
Robin Morel <rmorel@openstudio.fr>
2025-05-28 16:26:52 +0200

    remove mapping comp

--- assets/controllers/lang_select_controller.js
+++ assets/controllers/lang_select_controller.js
@@ -1,12 +1,8 @@
 import { Controller } from '@hotwired/stimulus';
-import { getComponent } from '@symfony/ux-live-component';
 
 class LangSelectController extends Controller {
   static targets = ['toggler'];
 
-  async initialize() {
-    this.component = await getComponent(this.element);
-  }
   connect() {
     this.element.addEventListener('keydown', ({ key }) => {
       if (key === 'Escape') {
