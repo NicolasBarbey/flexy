8e1c91613aa776db89782089412e27aea728dd1a
Robin Morel <rmorel@openstudio.fr>
2025-09-08 15:30:12 +0200

    fix store delivery card

--- assets/controllers/store_delivery_controller.js
+++ assets/controllers/store_delivery_controller.js
@@ -1,24 +1,15 @@
 import { Controller } from '@hotwired/stimulus';
 
 class StoreDeliveryController extends Controller {
-  connect() {
-    const buttons = document.querySelectorAll('.StoreDelivery-hours');
+  static targets = ['btn','list'];
 
-    buttons.forEach((button) => {
-      button.addEventListener('click', (el) => {
-        const parent = el.target.closest('.StoreDelivery');
-        const hoursListing = parent.querySelector(
-          '.StoreDelivery-hoursListing'
-        );
-        const isOpen = hoursListing.classList.toggle('md:hidden');
-        this.textShowHours(isOpen, button);
-      });
-    });
+  toggle({currentTarget}) {
+    const isOpen = this.listTarget.classList.toggle('md:hidden');
+    this.textShowHours(isOpen, currentTarget);
+
   }
-
   textShowHours(isOpen = true, button) {
-    const text = isOpen ? button.dataset.showHours : button.dataset.hideHours;
-    button.querySelector('span').textContent = text;
+    button.textContent = isOpen ? button.dataset.showHours : button.dataset.hideHours;
   }
 }
 
