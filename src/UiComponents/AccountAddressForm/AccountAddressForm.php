cbc636366b598b6f7dd95ffeffa722bc89b83c44
Robin Morel <rmorel@openstudio.fr>
2025-09-16 15:35:31 +0200

    fix ticket 37

--- src/UiComponents/AccountAddressForm/AccountAddressForm.php
+++ src/UiComponents/AccountAddressForm/AccountAddressForm.php
@@ -21,9 +21,12 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\ComponentWithFormTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Thelia\Api\Service\DataAccess\DataAccessService;
 use Thelia\Controller\Front\BaseFrontController;
 use Thelia\Domain\Addressing\Service\AddressService;
+use Thelia\Domain\Customer\CustomerFacade;
 use Thelia\Model\AddressQuery;
+use Thelia\Model\Customer;
 use TwigEngine\Service\FormService;
 
 #[AsLiveComponent(name: 'Flexy:AccountAddressForm', template: '@UiComponents/AccountAddressForm/AccountAddressForm.html.twig')]
@@ -36,16 +39,27 @@ class AccountAddressForm extends BaseFrontController
     public ?string $action = '';
     public ?string $successParam = '';
 
+    public ?Customer $customer;
+
     public function __construct(
         private readonly FormService $formService,
         private readonly AddressService $addressService,
         private readonly FormFactoryInterface $formFactory,
+        private readonly DataAccessService $dataAccessService,
+        private readonly CustomerFacade $customerFacade,
     ) {
     }
 
     protected function instantiateForm(): FormInterface
     {
-        $form = $this->formService->getFormByName(AddressEditForm::FORM_NAME, $this->getData());
+        $form = $this->formService->getFormByName(AddressEditForm::FORM_NAME, $this->getAddress());
+
+        if ($this->getCustomerHasOneAddress() && $this->addressId) {
+            $form->remove('is_default');
+            $form->add('is_default', HiddenType::class, [
+                'data' => 1,
+            ]);
+        }
 
         $form->add('error_url', HiddenType::class, [
             'data' => $this->requestStack->getCurrentRequest()->getUri(),
@@ -54,7 +68,7 @@ class AccountAddressForm extends BaseFrontController
         return $form;
     }
 
-    private function getData(): array
+    public function getAddress(): array
     {
         if (!$this->addressId) {
             return [];
@@ -62,5 +76,13 @@ class AccountAddressForm extends BaseFrontController
         $address = AddressQuery::create()->findPk($this->addressId);
 
         return $this->addressService->mapModelToFormData($address);
+    }
+
+    public function getCustomerHasOneAddress(): bool
+    {
+        $addresses = $this->dataAccessService->resources('/api/front/account/addresses', [
+            'customer.id' => $this->customerFacade->getCurrentCustomer()->getId(),
+        ]);
+        return \count($addresses) <= 1;
     }
 }
