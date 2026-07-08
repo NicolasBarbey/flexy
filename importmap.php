8de39cf673e9907cb6c0f3125ae5a954ff9104f1
David Nivot <dnivot@openstudio.fr>
2026-07-08 16:38:04 +0200

    WIP

--- importmap.php
+++ importmap.php
@@ -20,6 +20,24 @@ return [
         'version' => '3.2.2',
     ],
     '@symfony/stimulus-bundle' => [
-        'path' => '../../../vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+        'path' => '@symfony/stimulus-bundle/loader.js',
+    ],
+    'intl-messageformat' => [
+        'version' => '10.7.18',
+    ],
+    'tslib' => [
+        'version' => '2.8.1',
+    ],
+    '@formatjs/fast-memoize' => [
+        'version' => '2.2.7',
+    ],
+    '@formatjs/icu-messageformat-parser' => [
+        'version' => '2.11.4',
+    ],
+    '@formatjs/icu-skeleton-parser' => [
+        'version' => '1.8.16',
+    ],
+    '@symfony/ux-translator' => [
+        'path' => '@symfony/ux-translator/translator_controller.js',
     ],
 ];
